1f048cf9cb4b33a0c3aa85eb3ebfd12ede5ecf9e
Joseph Palmer <joseph@ryan-palmer.com>
2013-06-25 21:31:16 +0100

    Intial File Add
    Project V1 nearly finished, just need to dock to bottom right

---
+++ CommandCentre/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class comandcentre
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(comandcentre))
+        Me.taskman = New System.Windows.Forms.Button()
+        Me.ctrlpanel = New System.Windows.Forms.Button()
+        Me.shutdown = New System.Windows.Forms.Button()
+        Me.exitwindow = New System.Windows.Forms.Button()
+        Me.restart = New System.Windows.Forms.Button()
+        Me.abort = New System.Windows.Forms.Button()
+        Me.about = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'taskman
+        '
+        Me.taskman.Location = New System.Drawing.Point(6, 39)
+        Me.taskman.Name = "taskman"
+        Me.taskman.Size = New System.Drawing.Size(176, 27)
+        Me.taskman.TabIndex = 0
+        Me.taskman.Text = "Task Manager"
+        Me.taskman.UseVisualStyleBackColor = True
+        '
+        'ctrlpanel
+        '
+        Me.ctrlpanel.Location = New System.Drawing.Point(6, 6)
+        Me.ctrlpanel.Name = "ctrlpanel"
+        Me.ctrlpanel.Size = New System.Drawing.Size(176, 27)
+        Me.ctrlpanel.TabIndex = 1
+        Me.ctrlpanel.Text = "Control Panel"
+        Me.ctrlpanel.UseVisualStyleBackColor = True
+        '
+        'shutdown
+        '
+        Me.shutdown.Location = New System.Drawing.Point(6, 6)
+        Me.shutdown.Name = "shutdown"
+        Me.shutdown.Size = New System.Drawing.Size(176, 27)
+        Me.shutdown.TabIndex = 2
+        Me.shutdown.Text = "Shutdown"
+        Me.shutdown.UseVisualStyleBackColor = True
+        '
+        'exitwindow
+        '
+        Me.exitwindow.Location = New System.Drawing.Point(6, 6)
+        Me.exitwindow.Name = "exitwindow"
+        Me.exitwindow.Size = New System.Drawing.Size(176, 27)
+        Me.exitwindow.TabIndex = 3
+        Me.exitwindow.Text = "Exit"
+        Me.exitwindow.UseVisualStyleBackColor = True
+        '
+        'restart
+        '
+        Me.restart.Location = New System.Drawing.Point(6, 39)
+        Me.restart.Name = "restart"
+        Me.restart.Size = New System.Drawing.Size(176, 27)
+        Me.restart.TabIndex = 4
+        Me.restart.Text = "Restart"
+        Me.restart.UseVisualStyleBackColor = True
+        '
+        'abort
+        '
+        Me.abort.Location = New System.Drawing.Point(6, 72)
+        Me.abort.Name = "abort"
+        Me.abort.Size = New System.Drawing.Size(176, 27)
+        Me.abort.TabIndex = 5
+        Me.abort.Text = "Abort Shutdown"
+        Me.abort.UseVisualStyleBackColor = True
+        '
+        'about
+        '
+        Me.about.Location = New System.Drawing.Point(6, 39)
+        Me.about.Name = "about"
+        Me.about.Size = New System.Drawing.Size(176, 27)
+        Me.about.TabIndex = 6
+        Me.about.Text = "About"
+        Me.about.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(196, 134)
+        Me.TabControl1.TabIndex = 7
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.shutdown)
+        Me.TabPage1.Controls.Add(Me.restart)
+        Me.TabPage1.Controls.Add(Me.abort)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(188, 108)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Power"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.ctrlpanel)
+        Me.TabPage2.Controls.Add(Me.taskman)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(188, 202)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Advanced"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.exitwindow)
+        Me.TabPage3.Controls.Add(Me.about)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(188, 108)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "About"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'comandcentre
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(215, 154)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimizeBox = False
+        Me.Name = "comandcentre"
+        Me.Opacity = 0.75R
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Command Centre"
+        Me.TopMost = True
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents taskman As System.Windows.Forms.Button
+    Friend WithEvents ctrlpanel As System.Windows.Forms.Button
+    Friend WithEvents shutdown As System.Windows.Forms.Button
+    Friend WithEvents exitwindow As System.Windows.Forms.Button
+    Friend WithEvents restart As System.Windows.Forms.Button
+    Friend WithEvents abort As System.Windows.Forms.Button
+    Friend WithEvents about As System.Windows.Forms.Button
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+
+End Class
---
+++ CommandCentre/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class comandcentre
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles taskman.Click
+        Process.Start("taskmgr")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles exitwindow.Click
+        End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles ctrlpanel.Click
+        Process.Start("control")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles shutdown.Click
+        Process.Start("shutdown", "-s -t 10")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles restart.Click
+        Process.Start("shutdown", "-r -t 10")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles abort.Click
+        Process.Start("shutdown", "-a")
+    End Sub
+
+    Private Sub about_Click(sender As Object, e As EventArgs) Handles about.Click
+        MsgBox("Created by Joseph Ryan-Palmer")
+    End Sub
+
+    Private Sub comandcentre_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.StartPosition = FormStartPosition.Manual
+
+        Dim leftStart As Integer = Me.ClientSize.Width - (Me.Width + (SystemInformation.Border3DSize.Width * 2))
+        Dim topStart As Integer = Me.ClientSize.Height - (Me.Height + (SystemInformation.Border3DSize.Height * 2))
+
+        Me.Location = New Point(leftStart, topStart)
+        Me.Show()
+    End Sub
+End Class
